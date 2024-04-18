6ab541493e0c212b6484b0f8f3e03ab739548df4
denghe <denghe@hotmail.com>
2024-04-18 18:59:39 +0800

    writing...

--- _resmgr/src/db.cpp
+++ _resmgr/src/db.cpp
@@ -18,7 +18,6 @@ void DB::NewOrOpen(std::string path) {
 		FS::path p((std::u8string_view&)sv);
 		rootDir = p.remove_filename();
 		CreateTables();
-		CheckFiles();
 	}
 }
 
@@ -27,15 +26,69 @@ void DB::Close() {
 }
 
 void DB::CreateTables() {
-	// todo: if new ? create table
 	try {
+		conn.Execute(R"#(
+CREATE TABLE IF NOT EXISTS file (
+    id               INTEGER PRIMARY KEY AUTOINCREMENT,
+    name             TEXT    NOT NULL,
+    desc             TEXT,
+    preview_offset_x REAL,
+    preview_offset_y REAL,
+    preview_scale    REAL,
+    angle            REAL,
+    pivot_x          REAL,
+    pivot_y          REAL,
+    UNIQUE (
+        name COLLATE NOCASE ASC
+    )
+);
+)#");
 
-	}
-	catch (std::exception const& ex) {
-		xx::CoutN("throw exception after conn.Open. ex = ", ex.what());
+		conn.Execute(R"#(
+CREATE TABLE IF NOT EXISTS file_circle (
+    file_id INTEGER REFERENCES file (id),
+    idx INTEGER,
+    x       REAL    NOT NULL,
+    y       REAL    NOT NULL,
+    r       REAL    NOT NULL,
+    PRIMARY KEY (
+        file_id,
+        idx
+    )
+);
+)#");
+		conn.Execute(R"#(
+CREATE TABLE IF NOT EXISTS file_hook (
+    file_id INTEGER REFERENCES file (id),
+    idx INTEGER,
+    x       REAL    NOT NULL,
+    y       REAL    NOT NULL,
+    PRIMARY KEY (
+        file_id,
+        idx
+    )
+);
+)#");
+		conn.Execute(R"#(
+CREATE TABLE IF NOT EXISTS file_nail (
+    file_id INTEGER REFERENCES file (id),
+    idx INTEGER,
+    x       REAL    NOT NULL,
+    y       REAL    NOT NULL,
+    a_from  REAL,
+    a_to    REAL,
+    cfg     TEXT,
+    PRIMARY KEY (
+        file_id,
+        idx
+    )
+);
+)#");
+	} catch (std::exception const& ex) {
+		xx::CoutN("throw exception at CreateTables. ex = ", ex.what());
 	}
 }
 
-void DB::CheckFiles() {
-	// png file name must be unique
+bool DB::File::HasData() const {
+    return false;
 }
--- _resmgr/src/db.h
+++ _resmgr/src/db.h
@@ -1,12 +1,45 @@
 ﻿#pragma once
 #include "pch.h"
 
+
 struct DB {
+	struct FileCircle {
+		int32_t file_id, idx;
+		float x, y, r;
+	};
+	struct FileHook {
+		int32_t file_id, idx;
+		float x, y;
+	};
+	struct FileNail {
+		int32_t file_id, idx;
+		float x, y, a_from, a_to;
+		std::optional<std::string> cfg;
+	};
+	struct File {
+		int32_t id;
+		std::string name;
+		std::optional<std::string> desc;
+		std::optional<float> preview_offset_x;
+		std::optional<float> preview_offset_y;
+		std::optional<float> preview_scale;
+		std::optional<float> angle;
+		std::optional<float> pivot_x;
+		std::optional<float> pivot_y;
+
+		std::vector<FileCircle> circles;
+		std::vector<FileHook> hooks;
+		std::vector<FileNail> nails;
+
+		bool HasData() const;
+	};
+
 	std::filesystem::path rootDir;
 	xx::SQLite::Connection conn;
 	operator bool() const;
 	void NewOrOpen(std::string path);	// conn == true: success
 	void Close();
 	void CreateTables();
-	void CheckFiles();
+
+	// todo: Load Save File xxxxx
 };
